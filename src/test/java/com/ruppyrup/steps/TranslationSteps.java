3effabf57cce966911153551a8254afcdb080347
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-09-09 17:57:22 +0100

    Now get hit the translation service

--- src/test/java/com/ruppyrup/steps/TranslationSteps.java
+++ src/test/java/com/ruppyrup/steps/TranslationSteps.java
@@ -8,21 +8,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Slf4j
 @SpringBootTest(classes = TestConfig.class)
 public class TranslationSteps {
-
     private String urlUnderTest;
-
-    private String response;
 
     @Autowired
     private RestTemplate customRestTemplate;
+    private ResponseEntity<String> responseEntity;
 
     @Given("the {string} can be tested")
     public void theCanBeTested(String url) {
@@ -34,14 +37,40 @@ public class TranslationSteps {
     @When("a GET request is sent to the url")
     public void aGETRequestIsSentToTheUrl() {
         log.info("Performing get request to ::" + urlUnderTest);
-        ResponseEntity<String> responseEntity = customRestTemplate.getForEntity(urlUnderTest, String.class);
-        response = responseEntity.getBody();
+        ResponseEntity<String> response = customRestTemplate.getForEntity(urlUnderTest, String.class);
+        responseEntity = response;
     }
 
 
     @Then("the response {string} is received")
     public void theResponseIsReceived(String requiredResponse) {
         log.info("Checking the response is received ::" + requiredResponse);
-        assertThat(response).isEqualTo(requiredResponse);
+        assertThat(responseEntity.getBody()).isEqualTo(requiredResponse);
+    }
+
+    @When("a POST request is sent to the url with body {string}")
+    public void aPOSTRequestIsSentToTheUrlWithBodyMessageHelloFromRupert(String body) {
+        log.info("Performing POST request to the translation service");
+        String blogMessage = "Hello Rupert's from new blog";
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(mediaTypes);
+        HttpEntity<String> entity = new HttpEntity<>(blogMessage, headers);
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(8040)
+                .path("translation")
+                .build();
+
+        responseEntity = customRestTemplate.exchange(uriComponents.toString(), HttpMethod.POST, entity, String.class);
+    }
+
+    @Then("a response is received with status code {int}")
+    public void aResponseIsReceivedWithStatusCode(int statusCode) {
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(statusCode));
     }
 }
--- src/test/resources/features/TranslatorHappyPath.feature
+++ src/test/resources/features/TranslatorHappyPath.feature
@@ -2,6 +2,7 @@ Feature: Translator api tests
   This feature provides happy path tests for the translator api
 
   Scenario: can translate a message
-    Given the "https://www.google.co.uk" can be tested
-    When a GET request is sent to the url
-    Then the response "testResponse" is received
+    Given the "http://www.google.co.uk" can be tested
+    When a POST request is sent to the url with body "Hello Rupert's from new blog"
+    Then a response is received with status code 200
+    Then the response "Bonjour Rupert's de nouveau blog" is received
